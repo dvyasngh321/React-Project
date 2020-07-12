6d0a508dcbd29caaa6d8fd9f939181a441e7758d
Divya Singh <Singh.dvya05@gmail.com>
2020-07-12 16:28:46 +0545

    Updates

---
+++ precache-manifest.260425bc89332da91fe7647401f52b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3c0db033690b96d327909097c1ee0d",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "ea4239e5c8cb0a61a7bb",
+    "url": "/React-Project/static/css/main.85521671.chunk.css"
+  },
+  {
+    "revision": "0d7dd573742b002b5797",
+    "url": "/React-Project/static/js/2.80cc9e71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Project/static/js/2.80cc9e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea4239e5c8cb0a61a7bb",
+    "url": "/React-Project/static/js/main.583032b9.chunk.js"
+  },
+  {
+    "revision": "717b5df1717bdb38e6bf",
+    "url": "/React-Project/static/js/runtime-main.b92ef9d6.js"
+  }
+]);
